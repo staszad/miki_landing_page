6ae61afd2716e348560912058ddfa72d994e016c
staszad <stas_ady@protonmail.com>
2022-04-25 14:56:22 +0200

    minor changes in animations

--- js/main.js
+++ js/main.js
@@ -2,22 +2,59 @@ import anime from "./anime-master/lib/anime.es.js";
 
 let elements = document.getElementsByClassName("circle-abs");
 console.log(elements);
-let path = anime.path(".motion-path-demo path");
+var tl = anime.timeline({
+    easing: "easeOutExpo",
+});
 
-function randomValues() {
-    anime({
-        targets: elements,
+// Add children
+tl.add(
+    {
+        targets: "#c-red-1",
         translateX: function () {
             return anime.random(0, 50);
         },
         translateY: function () {
             return anime.random(0, 50);
         },
-        easing: "easeInOutSine",
         duration: function () {
             return anime.random(400, 2000);
         },
-        complete: randomValues,
-    });
-}
-randomValues();
+        loop: true,
+    },
+    0
+)
+    .add(
+        {
+            targets: "#c-yellow-1",
+            translateX: 250,
+            duration: 2000,
+        },
+        0
+    )
+    .add(
+        {
+            targets: "#c-black-1",
+            translateX: 250,
+            duration: 1000,
+        },
+        0
+    );
+
+// function randomValues() {
+anime({
+    targets: "#c-red-1",
+    easing: "linear",
+    translateX: function () {
+        return anime.random(0, 50);
+    },
+    translateY: function () {
+        return anime.random(0, 50);
+    },
+    duration: function () {
+        return anime.random(400, 2000);
+    },
+    direcrtion: "alternate",
+    loop: true,
+});
+// }
+// randomValues();
