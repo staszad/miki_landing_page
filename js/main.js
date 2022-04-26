a5e6f7eef592e675c56c95af7c6eac23e1f15816
staszad <stas_ady@protonmail.com>
2022-04-26 10:53:57 +0200

    probably finished the whole page

--- js/main.js
+++ js/main.js
@@ -2,59 +2,61 @@ import anime from "./anime-master/lib/anime.es.js";
 
 let elements = document.getElementsByClassName("circle-abs");
 console.log(elements);
-var tl = anime.timeline({
-    easing: "easeOutExpo",
-});
 
-// Add children
-tl.add(
-    {
-        targets: "#c-red-1",
+const maxLen = 50;
+const minTime = 1000;
+const maxTime = 2000;
+const easing = "easeInOutSine";
+// const easing = "cubicBezier(0.265, 0.425, 0.830, 0.430)";
+
+function randomValuesRed() {
+    anime({
+        targets: ["#c-red-1", "#c-red-2", "#c-red-3", "#c-red-4"],
+        easing: easing,
         translateX: function () {
-            return anime.random(0, 50);
+            return anime.random(0, maxLen);
         },
         translateY: function () {
-            return anime.random(0, 50);
+            return anime.random(0, maxLen);
         },
         duration: function () {
-            return anime.random(400, 2000);
+            return anime.random(minTime, maxTime);
         },
-        loop: true,
-    },
-    0
-)
-    .add(
-        {
-            targets: "#c-yellow-1",
-            translateX: 250,
-            duration: 2000,
+        complete: randomValuesRed,
+    });
+}
+function randomValuesBlack() {
+    anime({
+        targets: ["#c-black-1", "#c-black-2", "#c-black-3", "#c-black-4"],
+        easing: easing,
+        translateX: function () {
+            return anime.random(0, maxLen);
         },
-        0
-    )
-    .add(
-        {
-            targets: "#c-black-1",
-            translateX: 250,
-            duration: 1000,
+        translateY: function () {
+            return anime.random(0, maxLen);
         },
-        0
-    );
-
-// function randomValues() {
-anime({
-    targets: "#c-red-1",
-    easing: "linear",
-    translateX: function () {
-        return anime.random(0, 50);
-    },
-    translateY: function () {
-        return anime.random(0, 50);
-    },
-    duration: function () {
-        return anime.random(400, 2000);
-    },
-    direcrtion: "alternate",
-    loop: true,
-});
-// }
-// randomValues();
+        duration: function () {
+            return anime.random(minTime, maxTime);
+        },
+        complete: randomValuesBlack,
+    });
+}
+function randomValuesYellow() {
+    anime({
+        targets: ["#c-yellow-1", "#c-yellow-2", "#c-yellow-3", "#c-yellow-4"],
+        easing: easing,
+        translateX: function () {
+            return anime.random(0, maxLen);
+        },
+        translateY: function () {
+            return anime.random(0, maxLen);
+        },
+        duration: function () {
+            return anime.random(minTime, maxTime);
+        },
+        complete: randomValuesYellow,
+    });
+}
+randomValuesRed();
+randomValuesBlack();
+randomValuesYellow();
